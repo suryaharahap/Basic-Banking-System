ff478cddf76cd5fae9241e87dc1f46b9b034ad88
suryaharahap <suryaharahap18@gmail.com>
2023-10-19 21:26:23 +0700

    add: implement read file system

--- index.js
+++ index.js
@@ -2,4 +2,13 @@
 const os = require('os');
 const luasSegitiga = require('./segitiga.js');
 
+// Ini akan mengimport module bernama `fs`
+const fs = require('fs');
+
 console.log('Free Memory: ', os.freemem());
+
+// mengakses file system: melakukan read
+const isi = fs.readFileSync('./text.txt', 'utf-8');
+
+console.log(isi);
+// output: berdasarkan file txt
