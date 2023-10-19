7b28b96e7407ed53a0b76410129460235101cabe
suryaharahap <suryaharahap18@gmail.com>
2023-10-19 21:15:50 +0700

    learn third party module & learn import, export file

---
+++ index.js
@@ -0,0 +1,5 @@
+// Ini akan mengimport module bernama `os`
+const os = require('os');
+const luasSegitiga = require('./segitiga.js');
+
+console.log('Free Memory: ', os.freemem());
---
+++ segitiga.js
@@ -0,0 +1,6 @@
+// function ini kita sebut sebagai local module, yang bakal kita export
+function luasSegitiga(alas, tinggi) {
+  return (alas * tinggi) / 2;
+}
+
+module.exports = luasSegitiga;
